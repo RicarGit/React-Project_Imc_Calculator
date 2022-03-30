2090ed01bfd384f352465ff1f39553d19d67ce29
Ricardo Gava <ricardoagava@gmail.com>
2022-03-29 23:40:00 -0300

    Changed icon type of Level to up or down strings and typed the levels with an array of Level

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -1,11 +1,11 @@
 type Level = {
   title: string,
   color: string,
-  icon: string,
+  icon: 'up' | 'down',
   imc: number[]
 }
 
-const levels = [
+const levels: Level[] = [
   { title: 'Magreza', color: '#96A3AB', icon: 'down', imc: [0, 18.5] },
   { title: 'Normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9] },
   { title: 'Sobrepeso', color: '#E2B039', icon: 'down', imc: [25, 30] },
