14f901c33cb83874e970bc5e0bbcc6593341a195
Ricardo Gava <ricardoagava@gmail.com>
2022-04-02 09:54:13 -0300

    Passing the states to Left Arrow Component and display it based if the imc results exists

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -1,27 +1,31 @@
 import up from '../../assets/up.png'
 import down from '../../assets/down.png'
 import styles from './GridItem.module.css'
+import LeftArrowReset from '../ResetArrow/LeftArrowReset'
 import { Level } from '../../helpers/imc'
 
 type Props = {
-  item: Level
-  children?: number
+  item: Level,
+  setToShow?: any,
+  setHeight?: any,
+  setWeight?: any
 }
 
-function GridItem({ item, children }: Props) {
-  const { title, backgroundColor, icon, imc } = item
+function GridItem({ item, setToShow, setHeight, setWeight }: Props) {
+  const { title, backgroundColor, icon, imc, imcResult } = item
   const [minImcValue, maxImcValue] = imc
 
   return (
     <div className={styles.boxItem} style={{ backgroundColor }}>
-      <figure className={children ? styles.boxThumbIconBigger : styles.boxThumbIcon}>
-        <img className={children ? styles.thumbIconBigger : styles.thumbIcon} src={icon === 'up' ? up : down} alt="thumbs_image" />
+      <figure className={imcResult ? styles.boxThumbIconBigger : styles.boxThumbIcon}>
+        <img className={imcResult ? styles.thumbIconBigger : styles.thumbIcon} src={icon === 'up' ? up : down} alt="thumbs_image" />
       </figure>
-      <h2 className={children ? styles.boxTitleBigger : styles.boxTitle}>{title}</h2>
-      {children
-        ? <p className={styles.boxTextBigger}>O valor do seu IMC é: <strong>{children} Kg/m²</strong></p>
+      <h2 className={imcResult ? styles.boxTitleBigger : styles.boxTitle}>{title}</h2>
+      {imcResult
+        ? <p className={styles.boxTextBigger}>O valor do seu IMC é: <strong>{imcResult} Kg/m²</strong></p>
         : <p className={styles.boxText}>IMC está entre <strong>{minImcValue}</strong> e <strong>{maxImcValue}</strong></p>
       }
+      {imcResult && <LeftArrowReset item={item} setToShow={setToShow} setHeight={setHeight} setWeight={setWeight} />}
     </div>
   )
 }
