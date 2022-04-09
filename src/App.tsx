96335a0d3b8f8d2cf6a100994b4b8ae80aecc199
Ricardo Gava <ricardoagava@gmail.com>
2022-04-08 21:37:48 -0300

    Now the inputs were replaced by the new component CalcInputs the function related to it were moved to the component too, made some indentation refactoring on some areas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { levels, calculateImc, Level } from './helpers/imc'
+import { levels, Level } from './helpers/imc'
 import styles from './App.module.css'
 import logo from './assets/powered.png'
 import { GridItem } from './components/GridItem/GridItem';
+import CalcInputs from './components/CalcInputs/CalcInputs'
 
 function App() {
   const [heightField, setHeightField] = useState(0)
@@ -13,14 +14,6 @@ function App() {
   const handleWeight = (weight: number) => setWeightField(weight)
   const handleToShow = (level: Level | null) => setToShow(level)
 
-  const handleCalculateButton = () => {
-    if (heightField && weightField) {
-      return setToShow(calculateImc(heightField, weightField))
-    }
-
-    return alert('Digite seu peso e sua altura para calcular o seu IMC.')
-  }
-
   return (
     <div>
 
@@ -29,6 +22,7 @@ function App() {
       </header>
 
       <main className={styles.mainContainer}>
+
         <section className={styles.leftSide}>
           <h1>Calcule o seu IMC.</h1>
           <p>
@@ -36,28 +30,35 @@ function App() {
             adotado pela Organização Mundial de Saúde para
             calcular o peso ideal de cada pessoa.
           </p>
-          <input
-            type="number"
-            placeholder="Digite a sua altura: Ex: 1.5 (em metros)"
-            value={heightField <= 0 ? '' : heightField}
-            onChange={e => setHeightField(e.target.valueAsNumber)}
-            disabled={toShow ? true : false}
+
+          <CalcInputs
+            toShow={toShow}
+            height={heightField}
+            weight={weightField}
+            handleToShow={handleToShow}
+            handleHeight={handleHeight}
+            handleWeight={handleWeight}
           />
-          <input
-            type="number"
-            placeholder="Digite o seu peso: Ex: 50 (em kg)"
-            value={weightField <= 0 ? '' : weightField}
-            onChange={e => setWeightField(e.target.valueAsNumber)}
-            disabled={toShow ? true : false}
-          />
-          <button disabled={toShow ? true : false} onClick={handleCalculateButton}>Calcular</button>
         </section>
 
         <section className={!toShow ? styles.rightSide : styles.rightSideBig}>
           {!toShow && levels.map((item, index) => (
-            <GridItem key={index} item={item} toShow={handleToShow} height={handleHeight} weight={handleWeight} />
+            <GridItem
+              key={index}
+              item={item}
+              toShow={handleToShow}
+              height={handleHeight}
+              weight={handleWeight} />
           ))}
-          {toShow && <GridItem item={toShow} toShow={handleToShow} height={handleHeight} weight={handleWeight} />}
+
+          {toShow &&
+            <GridItem
+              item={toShow}
+              toShow={handleToShow}
+              height={handleHeight}
+              weight={handleWeight}
+            />
+          }
         </section>
 
       </main >
